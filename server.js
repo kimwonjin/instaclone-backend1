a83170415777223bb282d92b74361c5f2ff852ee
dopfit <80580430+dopfit@users.noreply.github.com>
2022-08-07 19:45:02 +0900

    nodemon--

---
+++ server.js
@@ -0,0 +1,21 @@
+const {ApolloServer, gql} =require("apollo-server");
+
+const typeDefs = gql`
+    type Query{
+        hello: String
+    }
+`
+
+const resolvers ={
+    Query:{
+        hello:() =>"worldbb",
+    },
+}
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen().then(() => console.log("server is running on http://localhost:4000/"))
